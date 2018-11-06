80df1509dd097d3e0a478de0d899fc5cd4b5410b
Derek Casas <Dvcasas@users.noreply.github.com>
2018-11-05 20:55:50 -0600

    Update Create-a-Variable:var.js

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Create-a-Variable:var.js
@@ -0,0 +1,9 @@
+Create a Variable: var
+
+var favoriteFood = 'pizza'
+var numOfSlices = 8
+console.log(favoriteFood)
+console.log(numOfSlices)
+
+/* The var keyword is used to declare an indistinct variable type. When a value is stored, the memory location
+adapts to the entered datatype. */
